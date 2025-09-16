f157522050f1edb90a93190e060b301490fff703
mahabubrahamanmiddya <mahabubmiddya7152@gmail.com>
2025-09-16 04:59:02 +0000

    conditions1

---
+++ ez/01_conditions.js
@@ -0,0 +1,12 @@
+// ++++++++++++++ Conditiond(if and else) ++++++++++++++++
+
+
+// write a age calculator
+
+let age = 2024 - 1970
+
+if (18 > age && age > 1) {console.log("He/She is a minor");
+}
+if else (age > 18 && 40 > age ) {
+    console.log("He/She is an Adult")
+}
